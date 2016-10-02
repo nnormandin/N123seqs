25d6c37abb5ddee8170d2a36fd1aa7de404d3459
nnormandin <nicholas.e.normandin@gmail.com>
2016-10-02 14:06:58 -0400

    Added HV analysis

--- scripts/analysis_script.R
+++ scripts/analysis_script.R
@@ -1,1 +1,29 @@
 # analysis script
+
+# bring into environment
+L_GAM <- read.table("~/N123seqs/data/EN51_N123primersonly_GAMKL_unique_final.txt")
+
+# examine size
+dim(L_GAM)
+
+# look at it
+head(L_GAM, 10)
+
+# rename columns
+colnames(L_GAM) <-c("Reads","H3nts","L3nts","H3aas","L3aas","HV",
+                    "HJ","HD","H3len","LV","LJ","L3len","Hiso","Liso")
+
+# check column types
+sapply(L_GAM, class)
+
+# plot HV
+plot(L_GAM$HV)
+
+# make HV a data table
+HV <- L_GAM$HV
+
+# check counts of HV and sort by descending
+HV_levels <- summary(HV)
+sort(HV_levels, decreasing = TRUE)
+
+
