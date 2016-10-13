bf5fb627333b53238709c6b9d28efb894c664633
enormandin <ericanormandin15@gmail.com>
2016-10-13 17:32:09 -0400

    Changed some file naming errors. Switched for correct gene of interest. Orientation of x axis labels

--- scripts/dplyr_analysis.R
+++ scripts/dplyr_analysis.R
@@ -4,8 +4,8 @@ library(tidyverse)
 L_GAM <- read.table("~/N123seqs/data/EN51_N123primersonly_GAMKL_unique_final.txt")
 FR_GAM <- read.table("~/N123seqs/data/EN50_N123_FR1-N123primers_GAMKL_unique_final.txt")
 LP_GAM <- read.table("~/N123seqs/data/EN52_N123_LP_GAMKL_unique_final.txt")
-L_GA <- read.table("~/N123seqs/data/EN53_N123_FR1-N123primers_GAKL_unique_final.txt")
-FR_GA <- read.table("~/N123seqs/data/EN54_N123primersonly_GAKL_unique_final.txt")
+L_GA <- read.table("~/N123seqs/data/EN54_N123primersonly_GAKL_unique_final.txt")
+FR_GA <- read.table("~/N123seqs/data/EN53_N123_FR1-N123primers_GAKL_unique_final.txt")
 LP_GA <- read.table("~/N123seqs/data/EN55_N123_LP_GAKL_unique_final.txt")
 
 # save as list
@@ -43,27 +43,27 @@ HV_analysis <- lapply(N123_libraries, AnalyzeHV)
 # you can now access HV counts like this:
 HV_analysis$L_GAM
 
-# you can get the number of IGHV1-18*01 in each library:
-IGHV1_18_counts <- as.numeric(sapply(HV_analysis,
-                                     function(x) x[x$HV == 'IGHV1-18*01', 2]))
+# you can get the number of IGHV1-2*02 in each library:
+IGHV1_2_counts <- as.numeric(sapply(HV_analysis,
+                                     function(x) x[x$HV == 'IGHV1-2*02', 2]))
 
-# you can get the proportion of IGHV1-18*01 (as a %) like this:
-IGHV1_18_props <- IGHV1_18_counts /
+# you can get the proportion of IGHV1-2*02 (as a %) like this:
+IGHV1_2_props <- IGHV1_2_counts /
   as.numeric(sapply(HV_analysis, function(x) sum(x$count))) * 100
 
 # plot these proportions
-plot(IGHV1_18_props, ylim = c(0,10), xaxt = 'n',
+plot(IGHV1_2_props, ylim = c(0,10), xaxt = 'n',
      xlab = '', type = 'h', ylab = 'Percentage',
-     main = 'Proportion of IGHV1-18*01 found in libraries')
-axis(1, at = seq(1,6), labels = names(N123_libraries))
+     main = 'Proportion of IGHV1-2*02 found in libraries')
+axis(1, at = seq(1,6), labels = names(N123_libraries),las=2)
 
 
 ############### ANALYZE H3LEN #####################
 
-# check H3len for IGHV1-18*01
+# check H3len for IGHV1-2*02
 AnalyzeH3len <- function(x){
   out <- x %>%
-    filter(HV == 'IGHV1-18*01') %>%
+    filter(HV == 'IGHV1-2*02') %>%
     arrange(desc(H3len)) %>%
     select(H3len)
   return(as.data.frame(out))}
@@ -86,20 +86,20 @@ dev.off()
 # return graph window back to single plot
 par(mfrow = c(1, 1))
 
-# what are the average H3len values for IGHV1-18*01 across libraries?
+# what are the average H3len values for IGHV1-2*02 across libraries?
 avg_H3len <- sapply(H3_len_values, function(x) mean(x$H3len))
 
 # you can plot it like this but they're all very similar
-plot(avg_H3len, main = 'Average H3len values for IGHV1-18*01',
+plot(avg_H3len, main = 'Average H3len values for IGHV1-2*02',
      xaxt = 'n', xlab = '', ylab = 'Avg H3len',
      ylim = c(0,18))
-axis(1, at = seq(1,6), labels = names(N123_libraries))
+axis(1, at = seq(1,6), labels = names(N123_libraries),las=2)
 
 ############# TOP HVs FROM LP_GAM ###################
 
 # get names of top 10 HVs from LP_GAM
 # this is really messy; look for a simpler way
-top_HVs <- as.character(unlist(as.data.frame(HV_analysis$LP_GAM[1:10,1])))
+top_HVs <- as.character(unlist(as.data.frame(HV_analysis$LP_GAM[1:15,1])))
 
 # write function to extract frequency of all HVs in a list from libraries
 ExtractHVFreqs <- function(x, HV_list){
@@ -125,7 +125,7 @@ top_HV_props <- top_HV_freqs /
 plot(top_HV_props$L_GAM, xaxt = 'n', ylab = 'Percentage', xlab = '',
      cex.lab = .7, cex.axis = .7, type = 'b',
      pch = 0, ylim = c(0, max(top_HV_props))) # square
-axis(side = 1, at = seq(1, 10), labels = top_HVs, cex.axis = .52)
+axis(side = 1, at = seq(1, 15), labels = top_HVs, cex.axis = .52,las=2)
 lines(top_HV_props$FR_GAM, type = 'b', pch = 1) # circle
 lines(top_HV_props$LP_GAM, type = 'b', pch = 2) # triangle
 lines(top_HV_props$L_GA, type = 'b', pch = 3) # plus
@@ -135,4 +135,4 @@ legend("topright", inset=.05, title="Library",
        names(N123_libraries), pch = seq(0,5), horiz=FALSE,
        cex = 0.7)
 
-# erica pull this
+# pulled yo - and on my work computer, no less
